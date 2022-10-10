28d1a05e3e972af2089a582d95ca8679556ef02c
shuhan <1552257473@qq.com>
2022-10-10 17:05:01 +0800

    第一次提交

---
+++ gulimall-ware/src/test/java/com/han/ware/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.han.ware.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
